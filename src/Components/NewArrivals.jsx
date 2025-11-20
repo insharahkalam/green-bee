fc0d7376420f8ece632dcec74a1aa2ce92511c23
insharahkalam <insharahkalam19@gmail.com>
2025-11-20 22:19:55 +0500

    update again

--- src/Components/NewArrivals.jsx
+++ src/Components/NewArrivals.jsx
@@ -33,7 +33,7 @@ const NewArrivals = () => {
     <a href="#">
         <h5 class="mt-6 mb-2 text-2xl font-semibold tracking-tight text-heading">Streamlining your design process today.</h5>
     </a>
-    <p class="mb-6 text-body">In today’s fast-paced digital landscape, fostering seamless collaboration among Developers and IT Operations.</p>
+    <p class="mb-6 text-body">In today&apos;s fast-paced digital landscape, fostering seamless collaboration among Developers and IT Operations.</p>
     <a href="#" class="inline-flex items-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
         Read more
         <svg class="w-4 h-4 ms-1.5 rtl:rotate-180 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4"/></svg>
@@ -44,6 +44,13 @@ const NewArrivals = () => {
 
                 </div>
 
+
+
+
+
+<div className='mt-10'>
+    <img src="https://greenbee-2.myshopify.com/cdn/shop/files/img-6_1920x1920.jpg?v=1613769390" alt="" />
+</div>
 
             </section>
         </>
