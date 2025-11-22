b7a525ae1758eb91d82248876e670d6c23294868
insharahkalam <insharahkalam19@gmail.com>
2025-11-22 11:56:13 +0500

    update

--- src/Components/NewArrivals.jsx
+++ src/Components/NewArrivals.jsx
@@ -20,25 +20,23 @@ const NewArrivals = () => {
                     </div>
                 </div>
 
-                <div className='card flex justify-center items-center gap-4 mt-5'>
+                <div className='card flex items-center gap-4 mt-5'>
 
 
+                    <div className='w-[220px] h-[300px] hover:shadow-2xl border rounded-xl'>
 
+                        <div>
+                            <img className='rounded-xl' src="https://greenbee-2.myshopify.com/cdn/shop/products/31_8c763282-1c0d-4fd7-a1b0-98628974d696_270x.jpg?v=1610525734" alt="" />
+                        </div>
 
+                        <div className='text-center'>
+                            <p className='text-lg tracking-wide text-gray-700'>Riamond Halo Stud Garrings</p>
+                            <span className='font-extrabold text-lg'>$5.00</span> <del className='font-extrabold text-lg text-gray-300 ms-5'>$14.00</del>
+                        </div>
 
-                    <div class="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs">
-                        <a href="#">
-                            <img class="rounded-base" src="https://greenbee-2.myshopify.com/cdn/shop/products/31_b3176b1f-f059-4fda-b675-52dad8b72b89_270x.jpg?v=1610525717" alt="" />
-                        </a>
-                        <a href="#">
-                            <h5 class="mt-6 mb-2 text-2xl font-semibold tracking-tight text-heading">Streamlining your design process today.</h5>
-                        </a>
-                        <p class="mb-6 text-body">In today&apos;s fast-paced digital landscape, fostering seamless collaboration among Developers and IT Operations.</p>
-                        <a href="#" class="inline-flex items-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
-                            Read more
-                            <svg class="w-4 h-4 ms-1.5 rtl:rotate-180 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4" /></svg>
-                        </a>
                     </div>
+
+
 
 
 
